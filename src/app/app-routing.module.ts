f2fc1fe68f2f05d28bece7731f368d4052590932
david1402 <49765318+david1402@users.noreply.github.com>
2021-08-16 10:51:39 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CityWeatherComponent} from './city-weather/city-weather.component';
+import { CityTilesContainerComponent } from './city-tiles-container/city-tiles-container.component';
+import { AddCityFormComponent } from './add-city-form/add-city-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CityTilesContainerComponent
+  },
+  {
+    path: 'name/:name',
+    component: CityWeatherComponent
+  },
+  {
+    path: 'new-city',
+    component: AddCityFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
